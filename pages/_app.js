5d56f4698b02560161abee2491ce33881771518d
Steven Yung <hyiuen@fastmail.com>
2021-01-13 16:45:02 +0100

    update title

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
 import '../styles/globals.css';
 import { NotifierContextProvider } from 'react-headless-notifier';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <NotifierContextProvider>
-      <Component {...pageProps} />
-    </NotifierContextProvider>
+    <>
+      <Head>
+        <title>No More Whatsapp</title>
+      </Head>
+
+      <NotifierContextProvider>
+        <Component {...pageProps} />
+      </NotifierContextProvider>
+    </>
   );
 }
 
